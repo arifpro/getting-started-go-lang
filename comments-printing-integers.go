f925755788f068dfb81a5dc1b443fb4e645bf0e4
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-13 20:59:48 +0800

    Create comments-printing-integers.go

---
+++ comments-printing-integers.go
@@ -0,0 +1,37 @@
+package main
+import "fmt"
+
+func main() {
+	// single line comment
+
+	/*
+	double line comment
+	*/
+
+	fmt.Printf("Hi")
+
+	name := "Khan"
+	fmt.Printf("HI" + name)
+	fmt.Println(name)
+	fmt.Print(name)
+
+	fmt.Printf("Hi %s", name)
+
+
+	// generic int declaration
+	var x int
+
+	// different lengths and signs
+	/*
+		int8, int16, int32, int64,
+		uint8, uint16, uint32, uint64
+	*/
+
+
+	// Binary operators
+	/*
+		Arithmetic: + - * / % << >>
+		Comparison: == != > < >= <=
+		Boolean: && ||
+	*/
+}
