1e8639652ad016b69ca80e4eb452cf0b3bcac3cf
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-13 21:49:18 +0800

    Create sting-packages.go

---
+++ sting-packages.go
@@ -0,0 +1,34 @@
+package main
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// Unicode Packages
+	/*
+		strings.IsDigit()
+		strings.IsSpace()
+		strings.IsLetter()
+		strings.IsLower()
+		strings.IsPunct()
+
+		strings.ToUpper()
+		strings.ToLower()
+
+		strings.HasPrefix()
+		strings.HasSuffix()
+
+		strings.Index()
+		strings.Replace()
+		strings.TrimSpace()
+	*/
+
+	// Strconv Package
+	/*
+		Atoi()  // converts string to int
+		itoa()  // converts int (base 10) to string
+		FormatFloat(f, fmt, prec, bitSize)  // converts floating point number to a string
+		ParseFloat (s, bitSize)  // Converts a string to a floating point number
+	*/
+}
